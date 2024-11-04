36fb2499229a27a1f338af8fe311c4d33f861608
yeehauc <yeehau.chew@gmail.com>
2024-11-05 01:06:02 +0800

    update database data
    update database data

--- database_backup.sql
+++ database_backup.sql
@@ -34,8 +34,42 @@ CREATE TABLE `alembic_version` (
 
 LOCK TABLES `alembic_version` WRITE;
 /*!40000 ALTER TABLE `alembic_version` DISABLE KEYS */;
-INSERT INTO `alembic_version` VALUES ('3c073acc11f0');
+INSERT INTO `alembic_version` VALUES ('7bba30e01006');
 /*!40000 ALTER TABLE `alembic_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int NOT NULL,
+  `role_name` varchar(50) NOT NULL,
+  `is_suspended` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_account_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `user_profile` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10008 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES (10001,'feb123231@fia.com','612474','a2234244','a23r23523',3,'Buyer',0),(10002,'feab12345@fia.comag','66538019','vvsfasawd','a23r23523',1,'Admin',0),(10003,'yeehau_123@hotmail.com','66538019','a23423523','a23r23523',3,'Seller',0),(10004,'feb2@fia.com','66538019','wgfwg1e1e','a23r23523',3,'Seller',0),(10005,'feb12345@fia.comag1','66538019','a23423523qgqre','scrypt:32768:8:1$0O7wocERrIL8uOQt$e444f9468e0eb2d29e103d74637273f49ff0f9e2ae8cbb3adc2d63afb95f0b74d68d0642bd4f13a998bf205ac5705c30c5103fd083fbf127b229187f53dd8fb6',2,'Buyer',0),(10006,'feb@fia.comwgw','66538019','test1234','scrypt:32768:8:1$Kr8FIxUU87iuVv8l$3f54b4b4cae3b1369d6d0ee2f6b7fe5042a8f749b12e7094ff22344b2da8a6118e448045b586995d161a1a5504433a2a83d579e91c7bca92b1a29e82afc1f4ba',1,'Admin',0),(10007,'admin@admin.cim','12345678','adminacc','scrypt:32768:8:1$otuA5h3NamiHN5rK$fe5a9636419370d785dddb75773c27478dfeed0cc4659aa4d72b6038418c60b51a85a33bf05f5a9acf9960a3867a5992f0459b49913f9ef9c78b8fe44e423676',1,'Admin',0);
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,7 +94,7 @@ CREATE TABLE `user_profile` (
 
 LOCK TABLES `user_profile` WRITE;
 /*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
-INSERT INTO `user_profile` VALUES (1,'Admin','user admin',0),(2,'Buyer','car buyer',0),(3,'Seller','Car seller',0),(4,'Agent','Used Car agent',1);
+INSERT INTO `user_profile` VALUES (1,'Admin','user admin',0),(2,'Buyer','car buyer',0),(3,'Seller','Car seller',0),(4,'Agent','Used Car Agent',1);
 /*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -73,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-19 21:41:16
+-- Dump completed on 2024-11-05  0:54:55
